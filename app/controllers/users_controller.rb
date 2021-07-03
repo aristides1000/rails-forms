821a91ebb37d81163a577540015dca2afedf0995
aristides1000 <aristides1000@gmail.com>
2021-07-03 18:15:22 -0400

    Finish Show MVC

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -13,7 +19,29 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def show; end
+
+  def update
+    respond_to do | format |
+      if @user.update(user_params)
+        format.html{redirect_to @user, notice: "User Updated"}
+      else
+        format.html{render :edit, notice: "#{@user.errors.full_messages.to_sentence}"}
+      end
+    end
+  end
+
+  def destroy
+
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
